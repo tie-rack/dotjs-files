2d208a605653ba6286c80f1ae4651c20eb135cf9
Chris Shea <chris.shea@patch.com>
2012-06-18 10:43:26 -0400

    Update xkcd title text find/show

--- xkcd.com.js
+++ xkcd.com.js
@@ -1,8 +1,9 @@
-var comic = $("div.s img[title]");
-var titleP = $("<p></p>");
+var comic = $("div#comic img[title]");
+var titleP = $("<div></div>");
 titleP.css({"font-variant": "normal",
             "border": "1px solid yellow",
             "padding": "1em",
+            "margin": "10px 20px",
             "background-color": "#FFFFCC"});
 titleP.text(comic.attr("title"));
 comic.after(titleP);
