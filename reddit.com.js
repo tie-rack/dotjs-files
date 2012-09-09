a5a3d640a9bf92b0b4eddf6cebe8a0f6e793fc1d
Chris Shea <cmshea@gmail.com>
2012-09-09 09:46:58 -0400

    Colorize notes

--- reddit.com.js
+++ reddit.com.js
@@ -3,16 +3,25 @@ var _TR = {
   authorId: function(e) { return $(e).attr("class").split(" ").filter(_TR.authorIdMatcher)[0]; },
   authorNote: function(authorId) { return localStorage.getItem(authorId); },
   authorSpanClass: function(authorId) { return "author-" + authorId; },
+  authorSpanHue: function(note) {
+    var hash = 1, i, char;
+    if (note.length == 0) return hash;
+    for (i = 0; i < note.length; i++) {
+      char = note.charCodeAt(i);
+      hash = hash + (char * char + char);
+    }
+    return hash % 360;
+  },
   authorSpan: function(authorId) {
     var note = _TR.authorNote(authorId);
     if (note) {
       var span = $("<span />");
-      span.css({"background": "green",
-                      "color": "white",
-                      "padding-left": "0.5em",
-                      "padding-right": "0.5em",
-                      "font-weight": "bold",
-                      "border-radius": "3px"});
+      span.css({"background": "hsl(" + _TR.authorSpanHue(note) + ", 100%, 25%)",
+                "color": "white",
+                "padding-left": "0.5em",
+                "padding-right": "0.5em",
+                "font-weight": "bold",
+                "border-radius": "3px"});
       span.text(note);
       span.addClass(_TR.authorSpanClass(authorId));
       return span;
