11896c26838f800fde8836a19ac7a40a59a13ae4
Chris Shea <cmshea@gmail.com>
2014-07-18 11:38:45 -0400

    Review and todo counts on duo

--- duolingo.com.js
+++ duolingo.com.js
@@ -1,8 +1,19 @@
-if(window.location.pathname === "/") {
-  var lessonsLeft = Array.prototype.map.call($(".lessons-left"),
-                                             function(e) { return e.innerHTML; }).
-                                               filter(function(e) { return e !== ""; }).
-                                               map(function(e) { return e.split("/"); }).
-                                               reduce(function(prev,curr,i,a) { return prev - parseInt(curr[0]) + parseInt(curr[1]); }, 0);
-  document.title = document.title + " (" + lessonsLeft + ")";
+function updateTitle() {
+  if(window.location.pathname === "/") {
+    var lessonsLeft = Array.prototype.map.call($(".lessons-left"),
+                                               function(e) { return e.innerHTML; }).
+      filter(function(e) { return e !== ""; }).
+      map(function(e) { return e.split("/"); }).
+      reduce(function(prev,curr,i,a) { return prev - parseInt(curr[0]) + parseInt(curr[1]); }, 0);
+
+    var strengtheningToDo = $(".strength-4").length +
+      $(".strength-3").length * 2 +
+      $(".strength-2").length * 3 +
+      $(".strength-1").length * 4 +
+      $(".strength-0").length * 5;
+
+    document.title = "Duo (" + strengtheningToDo + "|" + lessonsLeft + ")";
+  }
 }
+
+updateTitle();
