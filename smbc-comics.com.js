d9ff26f20c0af7dc1225945ddf70590f4cf12aea
Chris Shea <cmshea@gmail.com>
2015-03-04 12:52:37 -0500

    Fix SMBC for new layout

--- smbc-comics.com.js
+++ smbc-comics.com.js
@@ -1,16 +1,8 @@
-var comics = $('img[src^="http://www.smbc-comics.com/comics/"]');
+var comic = $("#comic");
 
-var comic = comics.filter(function(_, element) {
-  return ($(element).
-            attr('src').
-            match(/http:\/\/www.smbc-comics.com\/comics\/\d+\./));
-  });
-
-var aftercomic = comics.filter(function(_, element) {
-  return ($(element).
-          attr('src').
-          match(/http:\/\/www.smbc-comics.com\/comics\/\d+after\./));
-  }).clone();
+var aftercomic = $("#aftercomic").clone();
+aftercomic.css({"display": "block",
+                "position": "static"});
 
 var aftercomicContainer = $("<div></div>");
 aftercomicContainer.css({"background-color": "#CCC",
