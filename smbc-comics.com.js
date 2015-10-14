92353ace0c8e5e6036799547b0cbfe529a7a6037
Chris Shea <cmshea@gmail.com>
2015-10-14 15:26:20 -0400

    What? He started doing title tag jokes?!

--- smbc-comics.com.js
+++ smbc-comics.com.js
@@ -1,5 +1,17 @@
 var comic = $("#comic");
+var comicContainer = comic.parent();
 
+// Title tag!
+var titleP = $("<div></div>");
+titleP.css({"font-variant": "normal",
+            "border": "1px solid yellow",
+            "padding": "1em",
+            "margin": "10px 20px",
+            "background-color": "#FFFFCC"});
+titleP.text(comic.attr("title"));
+comicContainer.after(titleP);
+
+// Aftercomic!
 var aftercomic = $("#aftercomic").clone();
 aftercomic.css({"display": "block",
                 "position": "static"});
@@ -12,4 +24,4 @@ aftercomicContainer.css({"background-color": "#CCC",
                          "border": "1px solid #AAA"});
 aftercomicContainer.append(aftercomic);
 
-comic.after(aftercomicContainer);
+comicContainer.after(aftercomicContainer);
